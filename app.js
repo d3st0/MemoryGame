ccf5dc6e877a6b66173f99644777ec8872338f72
Balogh Tibor <d3st0.d3st0@gmail.com>
2024-05-01 22:45:13 +0200

    kész

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const pokeAPIBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
 const game = document.getElementById('game');
+let isPaused = false;
+let firstPick;
+let matches;
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+};
 
 const loadPokemon = async () => {
     const randomIds = new Set();
@@ -15,18 +34,75 @@ const loadPokemon = async () => {
 const displayPokemon = (pokemon) => {
     pokemon.sort(_ => Math.random() - 0.5);
     const pokemonHTML = pokemon.map(pokemon => {
+        const type = pokemon.types[0]?.type?.name || 'normal';
+        const color = colors[type];
         return `
-    <div class="card">
-    <h2>${pokemon.name}
-    </div>
+    <div class="card" style="background-color:${color}" onclick="clickCard(event)"
+                data-pokename="${pokemon.name}">
+                <div class="front">
+                </div>
+                <div class="back rotated" style="background-color:${color}">
+                    <h2>${pokemon.name}</h2>
+                    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}"/>
+                </div>
+            </div>
     `
     }).join('');
     game.innerHTML = pokemonHTML;
 }
 
-const resetGame = async () => {
-    const pokemon = await loadPokemon();
-    displayPokemon([...pokemon, ...pokemon]);
+const clickCard = (event) => {
+    const pokemonCard = event.currentTarget;
+    const [front, back] = getFrontAndBackFromCard(pokemonCard);
+    if (front.classList.contains("rotated") || isPaused) return;
+    isPaused = true;
+
+    rotateElements([front, back])
+    if (!firstPick) {
+        firstPick = pokemonCard;
+        isPaused = false;
+    } else {
+        const secondPokemonName = pokemonCard.dataset.pokename;
+        const firstPokemonName = firstPick.dataset.pokename;
+        if (firstPokemonName != secondPokemonName) {
+            const [firstFront, firstBack] = getFrontAndBackFromCard(firstPick);
+            setTimeout(() => {
+                rotateElements([front, back, firstFront, firstBack]);
+                firstPick = null;
+                isPaused = false;
+            }, 500);
+        } else {
+            matches++;
+            if (matches === 8) {
+            }
+            firstPick = 0;
+            isPaused = false;
+        }
+    }
 }
+
+const rotateElements = (elements) => {
+    if (typeof elements != 'object' || !elements.length) return;
+    elements.forEach(element => element.classList.toggle('rotated'));
+}
+
+const getFrontAndBackFromCard = (card) => {
+    const front = card.querySelector(".front");
+    const back = card.querySelector(".back");
+    return [front, back]
+}
+
+
+const resetGame = () => {
+    game.innerHTML = '';
+    isPaused = null;
+    firstPick = null;
+    matches = 0;
+    setTimeout(async () => {
+        const pokemon = await loadPokemon();
+        displayPokemon([...pokemon, ...pokemon]);
+        isPaused = false;
+    }, 200);
+}
+
 resetGame();
-loadPokemon();
